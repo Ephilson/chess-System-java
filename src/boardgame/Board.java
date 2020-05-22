131940764dd0e0288aca5166fee5240d12229620
Ephilson <edu.ugalde10@gmail.com>
2020-05-22 14:46:18 -0300

    board colors

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -12,7 +12,9 @@ public class Board {
 	//intancia a matriz de peças
 	
 	public Board(Integer rows, Integer columns) { // recebe da partida de xadrez o número de linhas e coluans
-		super();
+		if (rows <1 || columns <1) {
+			throw new BoardException("O tabuleiro de de xadrez deve ser uma matriz 8x8");
+		}
 		this.rows = rows; 
 		this.columns = columns;
 		pieces = new Piece[rows][columns]; // ao ser instanciado o tabuleio intancia também a matriz de peças de mesmo tamanho
@@ -20,20 +22,40 @@ public class Board {
 	public Integer getRows() {
 		return rows;
 	}
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
+
 	public Integer getColumns() {
 		return columns;
 	}
-	public void setColumns(Integer columns) {
-		this.columns = columns;
-	}
 	public Piece piece (int row, int column) { // diz a posição da peça
+		if (!positionExists(row,column)) {
+			throw new BoardException("posição inexistente!");
+		}
 		return pieces [row][column];
 	}
 	public Piece piece (Position position) {// busca a posição da peça
+		if (!positionExists(position)) {
+			throw new BoardException("posição inexistente!");
+		}
 		return pieces [position.getRow()][position.getColumn()];
 	}
 	
+	public void placePiece (Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("ja há uma peça na posição: "+position);
+		}
+		pieces [position.getRow()][position.getColumn()] = piece; // atribui a peça "piece" a matriz de peças "pieces"
+		piece.position = position; // atribui a posição "position" a atribuição de posição da peça
+	}
+	public boolean positionExists (int row, int column) {
+		return row >= 0 && row<rows && column >=0 && column < columns;
+	}
+	public boolean positionExists (Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("posição inexistente!");
+		}
+		return piece(position) !=null;
+	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +11,7 @@ public class ChessMatch {
 
 	public ChessMatch() {
 		board = new Board(8,8); // ao iniciar uma partida a partida incia um tabuleiro 8x8
+		initialSetup(); // incia a posição das peças
 	}
 	
 	public ChessPiece[][] getPieces(){ // a partida era buscar a posição das peças
@@ -21,5 +25,24 @@ public class ChessMatch {
 	}
 	
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
+	private void initialSetup() {
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
+	}
 	
 }
