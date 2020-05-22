aa72ece1a2f87f1552331bdc5df17a50a825e900
Ephilson <edu.ugalde10@gmail.com>
2020-05-22 15:22:10 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,6 +1,5 @@
 package boardgame;
 
-
 // instancia o tabuleiro
 public class Board {
 	
@@ -45,6 +44,19 @@ public class Board {
 		}
 		pieces [position.getRow()][position.getColumn()] = piece; // atribui a peça "piece" a matriz de peças "pieces"
 		piece.position = position; // atribui a posição "position" a atribuição de posição da peça
+	}
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("posição inexistente!");
+		}
+		if (piece(position)== null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+		
 	}
 	public boolean positionExists (int row, int column) {
 		return row >= 0 && row<rows && column >=0 && column < columns;
